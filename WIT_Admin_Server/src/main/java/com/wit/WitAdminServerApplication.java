0ec38497c91be61874a426da415827ff954e97e7
Minseo <wan7078@naver.com>
2024-08-09 15:45:52 +0900

    WIT_Admin 프로젝트 생성
    WIT_Admin 프로젝트 생성 ( Server )

---
+++ WIT_Admin_Server/src/main/java/com/wit/WitAdminServerApplication.java
@@ -0,0 +1,13 @@
+package com.wit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WitAdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WitAdminServerApplication.class, args);
+	}
+
+}
